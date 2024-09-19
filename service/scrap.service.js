b1dcfc3529f6730647c0132d7f57ce0998f1f784
friday096 <ssantoshh1996@gmail.com>
2024-09-20 00:21:10 +0530

    puppeter

--- service/scrap.service.js
+++ service/scrap.service.js
@@ -14,7 +14,9 @@ module.exports = {
         console.log('Backend URL:', url);
     
         try {
-            const browser = await puppeteer.launch();
+            const browser = await puppeteer.launch({
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            });
             const page = await browser.newPage();
             await page.goto(url, { waitUntil: 'networkidle2' });
 
